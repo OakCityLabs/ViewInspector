b56f4113e9c2fdcf3b459de20b39d9eec105019b
Sayam Patel <sayam@oakcity.io>
2020-07-31 16:59:40 -0400

    version bump

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ViewInspector",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v11), .iOS(.v14), .tvOS(.v13)
     ],
     products: [
         .library(
